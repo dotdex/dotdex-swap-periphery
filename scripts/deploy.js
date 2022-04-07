849bcf82e3523fef839ee0ddb65b81c573cb7d73
Altbee <liaojikundever@gmail.com>
2022-04-07 10:29:28 -0500

    update for dotdex

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,73 +1,67 @@
 function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 async function sleep() {
-  return await timeout(10000);
+  return await timeout(10000)
 }
 
 function get(chainId) {
-  const fs = require("fs");
+  const fs = require('fs')
 
-  const filename = '../wagyu-addresses/' + chainId + '.json'
+  const filename = '../dotdex-addresses/' + chainId + '.json'
 
-  const data = fs.existsSync(filename) ? JSON.parse(fs.readFileSync(filename, "utf8")) : {}
+  const data = fs.existsSync(filename) ? JSON.parse(fs.readFileSync(filename, 'utf8')) : {}
 
-  return data;
+  return data
 }
 
 function save(chainId, name, value) {
+  const fs = require('fs')
 
-  const fs = require("fs");
-
-  const filename = '../wagyu-addresses/' + chainId + '.json'
+  const filename = '../dotdex-addresses/' + chainId + '.json'
 
   const data = get(chainId)
 
-  data[name] = value;
+  data[name] = value
 
   fs.writeFileSync(filename, JSON.stringify(data, null, 4))
-
 }
 
-async function deploy(name, args=[]) {
-  console.log("deploy " + name, args)
-  const signers = await ethers.getSigners();
-  const nonce = await ethers.provider.getTransactionCount(signers[0]._address)
-  const { chainId } = await ethers.provider.getNetwork();
-  const Token = await ethers.getContractFactory(name);
-  const finalArgs = [...args, { nonce }] 
-  const token = await Token.deploy.apply(Token, finalArgs);
-  
-  save(chainId, name, token.address); 
-  await sleep()
-  console.log("deployed ", name, token.address);
-  return token.address
+async function deploy(name, args = []) {
+  console.log('deploy ' + name, args)
+  const signers = await ethers.getSigners()
+  // const nonce = await ethers.provider.getTransactionCount(signers[0]._address)
+  const { chainId } = await ethers.provider.getNetwork()
+  const Token = await ethers.getContractFactory(name)
+  // const finalArgs = [...args, { nonce }]
+  const finalArgs = [...args]
+  const token = await Token.deploy.apply(Token, finalArgs)
 
+  save(chainId, name, token.address)
+  await sleep()
+  console.log('deployed ', name, token.address)
+  return token.address
 }
 
 async function main() {
   // We get the contract to deploy
 
-  const signers = await ethers.getSigners();
+  const signers = await ethers.getSigners()
 
-  const { chainId } = await ethers.provider.getNetwork();
-  
+  const { chainId } = await ethers.provider.getNetwork()
+
   const data = get(chainId)
 
-  if (data.WagyuFactory && data.WVLX) {
-    await deploy("WagyuRouter", [data.WagyuFactory, data.WVLX])
+  if (data.DotDexFactory && data.WGLMR) {
+    await deploy('DotDexRouter', [data.DotDexFactory, data.WGLMR])
+  } else {
+    throw 'data.DotDexFactory and data.WGLMR are expected to be available'
   }
-  else {
-    throw "data.WagyuFactory and data.WVLX are expected to be available"
-  }
-
-  
-
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
